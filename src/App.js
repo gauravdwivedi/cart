9a73f95c7580b4425254eb6f77f5e6b92719b666
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-04-14 11:01:55 +0530

    added product through add method in firebase

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
       products: [],
       loading: true
     }
+
+    this.db = firebase.firestore();
+    
 }
 
   componentDidMount() {
@@ -36,8 +39,7 @@ class App extends React.Component {
     //       loading: false
     //     })
     // })
-    firebase
-     .firestore()
+    this.db
     .collection('products')
     .onSnapshot((snapshot) => {
       console.log(snapshot);
@@ -119,12 +121,32 @@ handleDeleteProduct = (id) => {
     })
     return cartTotal;
   }
+
+  addProduct = () => {
+    this.db.collection(
+      'products'
+    )
+      .add({
+        img: '',
+        price: 900,
+        qty: 4,
+        title:'washing machine'
+      })
+      .then((docRef) => {
+        console.log("Prodcut has been added", docRef);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+    })
+      
+  }
   render() {
     const { products, loading } = this.state;
     return (
       <div className="App">
 
         <Navbar count={this.getCartCount()} />
+        <button onClick={this.addProduct}>Add a Product</button>
         <Cart
           products ={products}
          onIncreaseQuantity={this.handleIncreaseQuantity}
